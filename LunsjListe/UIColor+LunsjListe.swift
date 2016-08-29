960bc84dec5827abcfdf37de2359850f11363613
Fredrick Biering <fredrickb@mesan.no>
2016-08-29 19:15:17 +0200

    Properly validates username being set and displays warning if no username has been entered. Button changes color and text when user enters username and clicks the button.

---
+++ LunsjListe/UIColor+LunsjListe.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+LunsjListe.swift
+//  LunsjListe
+//
+//  Created by Fredrick Biering on 29/08/16.
+//  Copyright © 2016 Fredrick Biering. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    class func darkGreen() -> UIColor {
+        return UIColor(red: 27 / 255, green: 146 / 255, blue: 74 / 255, alpha: 1.0)
+    }
+}
--- LunsjListe/ViewController.swift
+++ LunsjListe/ViewController.swift
@@ -13,9 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var weekdayLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
     
     @IBAction func onClick(_ sender: UIButton) {
-        sender.backgroundColor = UIColor.green
+        warningLabel.isHidden = true
+        
+        let username = usernameTextfield.text
+        let valid = validateUsername(username: username)
+        
+        if (valid) {
+            sender.backgroundColor = UIColor.darkGreen()
+            sender.isEnabled = false
+        } else {
+            warningLabel.isHidden = false
+        }
+        
+        print("valid is: \(valid)")
+    }
+    
+    func validateUsername(username: String?) -> Bool {
+        return !username!.isEmpty
     }
     
     override func viewDidLoad() {
